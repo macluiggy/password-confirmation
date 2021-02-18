fbb054259b6b864ead28fc80846b0c710ddbbdfe
Luiggy Macias <sosoteluiggy@gmail.com>
2021-02-18 17:11:50 -0500

    update index.html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 (function () {
     var password = document.getElementById('pswrd');
     var confPassword = document.getElementById('confpswrd');
-    var errmsg = document.getElementById('err')
+    var errmsg = document.getElementById('err');
+    var elButton = document.getElementById('change');
 
     confPassword.addEventListener('blur', cPassword, false);
     function cPassword() { //confirma que el valor de ambas contraseñas sean iguales
         if (password.value !== confPassword.value) { //si sus valores son diferentes
             errmsg.innerHTML = 'Passwords don\'t match'; // muestra el mensaje de error
         } else {
-            errmsg.innerHTML = ''; // si no no muestres nada
+            errmsg.innerHTML = ''; // si no, no muestres nada
         }
+    }
+
+    elButton.addEventListener('click');
+    function (e) {
+        e.preven
     }
 }());
